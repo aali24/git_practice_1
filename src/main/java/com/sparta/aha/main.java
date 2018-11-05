4a4b01c7ec6961777ab0f6396b566d7de703dd41
abdi ali <aali@spartaglobal.com>
2018-11-05 10:39:26 +0000

    source initial commit

---
+++ src/main/java/com/sparta/aha/main.java
@@ -0,0 +1,12 @@
+package com.sparta.aha;
+
+
+public class main
+{
+    public static void main( String[] args )
+    {
+
+
+
+    }
+}
---
+++ src/test/java/com/sparta/aha/AppTest.java
@@ -0,0 +1,20 @@
+package com.sparta.aha;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple main.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
